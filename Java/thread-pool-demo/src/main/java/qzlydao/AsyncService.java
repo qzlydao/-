2dd085b4b1be7172d874e99546957d144e8edd70
qzlydao <qzlydao@126.com>
2021-12-06 14:16:31 +0800

    add java files

---
+++ Java/thread-pool-demo/src/main/java/qzlydao/AsyncService.java
@@ -0,0 +1,15 @@
+package qzlydao;
+
+/**
+ * @Author: liuqiang
+ * @Date: 2021/12/6/006 13:43
+ * @Desc:
+ */
+public interface AsyncService {
+
+    String execute(String message);
+
+    String doSomething(String message);
+
+
+}
